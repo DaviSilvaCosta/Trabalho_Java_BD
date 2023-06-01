e40d611259a867080fc0e359e85ad8297cad947e
Davi_Costa <135281163+DaviSilvaCosta@users.noreply.github.com>
2023-06-01 20:05:43 -0300

    Criando Drive SQL (parte 1)

---
+++ ConectantoSQL1.java
@@ -0,0 +1,13 @@
+public class ConectandoSQL1{
+  static String driverJDBC = "org.postgresql.Driver";
+  public static void main(String[] args){
+    
+    try{
+      System.out.println("Carregando driver JDBC....");
+      Class.forName(driverJDBC);
+      System.out.println("Driver carregado!!");
+    }catch (Exception e){
+      System.out.printf("Falha no carregamento! %c", e);
+    }
+  }
+}
